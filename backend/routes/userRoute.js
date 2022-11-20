70af14317f90b0716755640dc5bc5a4342376526
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-11-20 15:57:09 +0530

    completed backend except forgot password

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const router =express.Router();
+const { registerUser, loginUser, logOut,getUser, loginStatus, updateUser,changePassword, forgotPassword }=require("../controllers/userController");
+const protect=require("../middleWare/authmiddleWare")
+router.post("/register",registerUser);
+router.post("/login",loginUser);
+router.get("/logout",logOut);
+router.get("/getuser",protect, getUser);
+router.get("/loggedin",loginStatus);
+router.patch("/updateuser",protect, updateUser);
+router.patch("/changepassword",protect, changePassword);
+router.post("/forgotpassword",forgotPassword);
+module.exports=router;
