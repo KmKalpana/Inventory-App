6ea1718ca84110a39a33d1b7d47f729aa0f7c582
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-11-22 19:50:41 +0530

    updated

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import {RiProductHuntLine} from 'react-icons/ri'
-import { Link } from 'react-router-dom';
-import './Home.scss'
+import React from "react";
+import { RiProductHuntLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+
 const Home = () => {
-    return (
-        <div className='Home'>
-          <nav className='container --flex-between'>
-          <div className='logo'>
-             <RiProductHuntLine size={35}/>
-          </div>
-          <ul className='home-links'>
-            <li>
-            <Link to="/register">
-              Register
-            </Link>
-         </li>
-           <li>
-           <button className='--btn --btn-primary'>
-            <Link to="/login">
-              Login
-            </Link>
-            </button>
-         </li>
-         <li>
-           <button className='--btn --btn-primary'>
-            <Link to="/dashboard">
-              dashboard
-            </Link>
-            </button>
-         </li>
-          </ul>
-          </nav>  
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo">
+          <RiProductHuntLine size={35} />
         </div>
-    );
-}
+
+        <ul className="home-links">
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+        </ul>
+      </nav>
+     {/* Hero Section */}
+     <section className="container">
+     </section>
+    </div>
+  );
+};
 
 export default Home;
