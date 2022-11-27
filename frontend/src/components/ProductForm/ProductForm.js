48142ad5e4d489cd84df5d8edcb13aaf7ea3e42d
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-11-27 11:58:56 +0530

    ProductForm Page Completed

--- frontend/src/components/ProductForm/ProductForm.js
+++ frontend/src/components/ProductForm/ProductForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 const ProductForm = (product,productImage,imagePreview,description,setDescription,handleInputChange,handleImageChange,saveProduct) => {
   return <div></div>
 }
